efe41bb1c6de622efc8ad4212b7f8e9b6f4e6898
cahinosilva.20184 <cahinosilva.20184@LABI2-13PC05>
2023-02-27 13:59:42 +0100

    Aggiungere i file di progetto.

---
+++ es.2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace es._2
+{
+    internal class Program
+    {
+        static void confronto(ref DateTime data, ref int []a)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            int[] a=new int[12];
+            Console.WriteLine("Inserire data di partenza");
+            DateTime data = new DateTime();
+            Console.WriteLine(data.ToString(Console.ReadLine()));
+            Console.WriteLine("Inserire tariffe:");
+            for(int i=1; i<13; i++)
+            {
+                Console.WriteLine("Inserire tariffa mese "+i);
+                a[i]=int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
